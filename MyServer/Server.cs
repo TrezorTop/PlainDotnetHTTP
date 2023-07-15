35995834b37c3e16fe5f9ae3f3fedf1329e6f05d
trezortop92@gmail.com <trezortop92@gmail.com>
2023-07-15 13:18:06 +0500

    Initial

---
+++ MyServer/Server.cs
@@ -0,0 +1,30 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace MyServer;
+
+public class Server
+{
+    public void Start()
+    {
+        TcpListener listener = new TcpListener(IPAddress.Any, 5000);
+        listener.Start();
+
+        while (true)
+        {
+            TcpClient client = listener.AcceptTcpClient();
+
+            using NetworkStream networkStream = client.GetStream();
+            using StreamReader reader = new StreamReader(networkStream);
+            using StreamWriter writer = new StreamWriter(networkStream);
+
+            string line = reader.ReadLine(); 
+            while (line != string.Empty)
+            {
+                Console.WriteLine(reader.ReadLine());
+            }
+
+            writer.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ PlainDotnetHTTP/Program.cs
@@ -0,0 +1,4 @@
+﻿using MyServer;
+
+Server server = new Server();
+server.Start();
